3e53467a717918a4d0758c67ca707b42dd78b668
maliacho <maliacho21@gmail.com>
2023-09-29 23:07:37 -0700

    started to refine routes files

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,13 +5,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
+  // be sure to include its associated Product data
   try{
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }]
+    });
     res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Product data
 });
 
 router.get('/:id', async (req, res) => {
@@ -19,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: ''}]
+      include: [{ model: Product, through: ProductTag}]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No product with this tag.'});
@@ -48,6 +50,7 @@ router.put('/:id', (req, res) => {
       id: req.params.id,
     },
   })
+  res.status(200).json()
 });
 
 router.delete('/:id', async (req, res) => {
